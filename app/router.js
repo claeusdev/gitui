7be1a1a32b07e8d2442abb38915144e4fc5f2b90
Nana Adjei Manu <claeusdev@outlook.com>
2019-07-10 07:23:57 +0000

    Add routes

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
@@ -7,6 +7,16 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route("home");
+  this.route("orgs");
+  this.route("org", { path: "org/:id" }, function() {
+    this.route("repos");
+    this.route("repo", { path: ":repoid" }, function() {
+      this.route("contributors");
+      this.route("issues");
+    });
+  });
+  this.route("notfound", { path: "*path" });
 });
 
 export default Router;
